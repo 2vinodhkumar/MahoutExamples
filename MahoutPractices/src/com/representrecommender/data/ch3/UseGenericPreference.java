b81a4aefe92a9e260ca1140717865a530f7da6c0
Vinodh <2vinodhkumar@gmail.com>
2014-09-10 17:51:44 +0530

    chapter3

---
+++ MahoutPractices/src/com/representrecommender/data/ch3/UseGenericPreference.java
@@ -0,0 +1,13 @@
+package com.representrecommender.data.ch3;
+
+import org.apache.mahout.cf.taste.impl.model.GenericPreference;
+
+public class UseGenericPreference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new GenericPreference(1, 101, 3.2f));
+		
+	}
+
+}
