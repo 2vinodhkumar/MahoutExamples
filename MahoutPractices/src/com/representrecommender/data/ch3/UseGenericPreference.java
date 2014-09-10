8b6feff90867a52c1de6b89cda4c1a0ed6b5cc95
Vinodh <2vinodhkumar@gmail.com>
2014-09-10 18:01:28 +0530

    commented

--- MahoutPractices/src/com/representrecommender/data/ch3/UseGenericPreference.java
+++ MahoutPractices/src/com/representrecommender/data/ch3/UseGenericPreference.java
@@ -6,6 +6,8 @@ public class UseGenericPreference {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//GenericPreference is implementation of Preference interface args=userID,ItemID,preference value
 		System.out.println(new GenericPreference(1, 101, 3.2f));
 		
 	}
